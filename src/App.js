d414227daaec38d822a86b7f06a2892e90800dc7
Hamza Khan <hamzakh1010@gmail.com>
2020-11-10 01:08:04 +0500

    filter func add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import "./App.css";
 const App = () => {
   const [coins, setCoins] = useState();
   const [search, setSearch] = useState("");
